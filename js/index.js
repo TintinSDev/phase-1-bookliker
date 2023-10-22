86fbb6ca38c706c159950b6335072c2d30dd0111
Martin <mmaina290@gmail.com>
2023-10-22 11:54:19 +0300

    booker liker

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const BOOKS_URL = "http://localhost:3000/books";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const list = document.querySelector("#list")
+    list.addEventListener("click", function (event) {
+        event.preventDefault();
+        fetch(BOOKS_URL, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({list})
+        }).then(response => {
+            return response.json();
+        }).then(data => {
+            showPanel.innerHTML = data;
+        })
+    })
+    
+});
